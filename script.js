5c764b70fc4727375fd0ed80f9db1de6c93a8deb
GabrielCanarin <bielsalaz07@gmail.com>
2023-03-23 12:50:18 -0300

    atualizado js, mensagem nulas não serao enviadas e a tecla enter pode enviar mensagens

--- script.js
+++ script.js
@@ -3,13 +3,24 @@ const text = document.querySelector(".txt_input");
 const chatarea = document.querySelector(".chat_area");
 
 function attchat() {
-  var menssagem = text.value;
+  var mensagem = text.value;
+
+  if (mensagem === "") {
+    return
+  }
 
   var div = document.createElement("div");
   div.className = "user";
 
   div.innerHTML = `<p class="identific">Você diz:</p>
-                   <p class="mensagem">${menssagem}</p>`;
+                   <p class="mensagem">${mensagem}</p>`;
 
   chatarea.appendChild(div);
+  text.value = "";
 }
+
+text.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    attchat();
+  }
+});
